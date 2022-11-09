b90555ec0b0d296372b5bcf01e98778703c57907
Kareem Talbert <talbert.kareem@gmail.com>
2022-11-08 23:56:20 -0500

    removed ubuntu filters

--- aws/terraform-saltstack/modules/compute/main.tf
+++ aws/terraform-saltstack/modules/compute/main.tf
@@ -6,24 +6,8 @@ variable "public_subnet" {
    description = "The public subnet IDs assigned to the salt server"
 }
 
-data "aws_ami" "ubuntu" {
-   most_recent = "true"
-
-   filter {
-      name = "name"
-      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-   }
-
-   filter {
-      name = "virtualization-type"
-      values = ["hvm"]
-   }
-
-   owners = ["099720109477"]
-}
-
 resource "aws_instance" "salt_server" {
-   ami = "ami-0c5a1c466350aa024" #"ami-09d3b3274b6c5d4aa" #data.aws_ami.ubuntu.id
+   ami = "ami-09d3b3274b6c5d4aa"
    subnet_id = var.public_subnet
    instance_type = "t2.micro"
    private_ip = "10.10.1.234"
@@ -52,7 +36,7 @@ resource "aws_eip" "salt_eip" {
 
 resource "aws_instance" "salt_minion" {
   depends_on = [aws_instance.salt_server]
-  ami = "ami-0c5a1c466350aa024" #"ami-09d3b3274b6c5d4aa"
+  ami = "ami-09d3b3274b6c5d4aa"
   subnet_id = var.public_subnet
   instance_type = "t2.micro"
   vpc_security_group_ids = [var.security_group]
